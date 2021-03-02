eeb6080856e7e841446faeaeae0c5bc53a7a0a04
lradu <l.radu01@yahoo.com>
2021-03-02 14:40:05 +0200

    a

---
+++ week2/src/ro/ase/csie/cts/g1094/laboratrory/exceptions/IllegalTransferException.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.g1094.laboratrory.exceptions;
+
+public class IllegalTransferException extends Exception {
+
+}
---
+++ week2/src/ro/ase/csie/cts/g1094/laboratrory/exceptions/InsufficientFundsException.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1094.laboratrory.exceptions;
+
+public class InsufficientFundsException extends Exception {
+
+	
+	public InsufficientFundsException(String message) {
+		super(message);
+	}
+	
+	public InsufficientFundsException() {
+		
+	}
+}
