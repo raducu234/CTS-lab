0d54e2670091267a59cca199df835caee861fe0a
lradu <l.radu01@yahoo.com>
2021-04-01 15:20:32 +0300

    -dasd

--- Week6-Prototype builder/src/ro/ase/csie/cts/g1093/dp/builder/SuperHero.java
+++ Week6-Prototype builder/src/ro/ase/csie/cts/g1093/dp/builder/SuperHero.java
@@ -23,6 +23,47 @@ public SuperHero(String name, int lifePoints, boolean isVillain, boolean isWound
 	this.rightWeapon = rightWeapon;
 	this.leftWeapon = leftWeapon;
 }
- 
+ public static class SuperHeroBuilder{
+	 SuperHero superhero;
+	 
+	 public SuperHeroBuilder(String name ,int lifePoints) {
+		 this.superhero=new SuperHero();
+		 this.superhero.name=name;
+		 this.superhero.lifePoints=lifePoints;
+	 }
+	 
+	 
+	 public SuperHeroBuilder isVillain() {
+		 this.superhero.isVillain=true;
+		 return this;
+	 }
+	 public SuperHeroBuilder isWounded() {
+		 this.superhero.isWounded=true;
+		 return this;
+	 }
+	 
+	 public SuperHeroBuilder setRightWeapon(WeaponIntertface weapon) {
+		 this.superhero.rightWeapon=weapon;
+		 return this;
+	 }
+	 
+	 public SuperHeroBuilder setLeftWeapon(WeaponIntertface weapon) {
+		 this.superhero.leftWeapon=weapon;
+		 return this;
+	 }
+	 
+	 public SuperHeroBuilder setSuperPower(SuperPowerInterface power) {
+		 this.superhero.superheroPower=power;
+		 return this;
+	 }
+	 
+	 public SuperHero build() {
+		 return new s SuperHero(null,0,false,flase,null,null,null,null);
+		 return this.superhero;
+	 }
+	 
+	 
+	 
+ }
  
 }
--- Week6-Prototype builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
+++ Week6-Prototype builder/src/ro/ase/csie/cts/g1093/dp/builder/TestBuilder.java
@@ -5,11 +5,15 @@ public class TestBuilder {
 	public static void main(String[] args) {
 		
 		//1.create the object
-		SuperHero superHero=new SuperHero();
+		//SuperHero superHero=new SuperHero();
 		//2.init it
 		
+		//
+		//SuperHero superHero2= new SuperHero("Superman",100,false,new Weapon(),null,new Flying(),null);
 		
-		SuperHero superHero2= new SuperHero("Superman",100,false,new Weapon(),null,new Flying(),null);
+		SuperHero superman=new SuperHero.SuperHeroBuilder("Superman", 100).build();
+		
+		SuperHero joker=new SuperHero.SuperHeroBuilder("Joker", 200).isVillain().isWounded().setRightWeapon(new Weapon).build();
 	}
 
 }
