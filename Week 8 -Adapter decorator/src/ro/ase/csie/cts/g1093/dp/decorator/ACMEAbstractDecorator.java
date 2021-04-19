21830518a14507636dbc615b13f48fc81be4e732
lradu <lradu@DESKTOP-R41TOH0>
2021-04-19 10:27:53 +0300

    dssadd

--- Week 8 -Adapter decorator/src/ro/ase/csie/cts/g1093/dp/decorator/ACMEAbstractDecorator.java
+++ Week 8 -Adapter decorator/src/ro/ase/csie/cts/g1093/dp/decorator/ACMEAbstractDecorator.java
@@ -19,6 +19,8 @@ public abstract class ACMEAbstractDecorator extends ACMECharacter {
 		this.character.takeAHit(points);
 	}
 	public void heal(int points) {
+		
+		
 		this.character.heal(points);
 		
 		
