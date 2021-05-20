b36c22282164eb379290df2aab2466bff594aad8
lradu <lradu@DESKTOP-R41TOH0>
2021-05-20 15:34:14 +0300

    Implementing unit testin g

---
+++ Week 12 -Unit Testing/src/ro/ase/csie/main/MainApp.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.main;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+	System.out.println("The app is running...");
+		
+		
+//		int a = 5;
+//		int b = 6;
+//		int exceptedResult = 11;
+//		int actualResult = MathOperations.add(a, b);
+//		if(actualResult == exceptedResult) {
+//			System.out.println("It works");
+//		}
+//		else
+//		{
+//			System.out.println("I need more math classes");
+//		}
+
+	}
+
+}
